72c3142f794e62f2095c290feac5a769cbf8f078
sersim33 <sersim33@gmail.com>
2024-03-03 16:49:52 +0200

    com1

---
+++ homework2.py
@@ -0,0 +1,8 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+
+    return inner
