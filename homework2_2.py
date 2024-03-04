2592baac4e31df1cfed1f27b9afbc81a3ea63df5
sersim33 <sersim33@gmail.com>
2024-03-04 10:27:51 +0200

    com2

--- homework2_2.py
+++ homework2_2.py
@@ -8,21 +8,31 @@ class Field:
         return str(self.value)
 
 class Name(Field):
-    # реалізація класу
-    pass     
-class Phone(Field):
     pass 
-    # реалізація класу
+
+class Phone(Field):
+    def __init__(self, phone):
+        if len(phone) == 10 and phone.isdigit():
+            super().__init__(phone)
+        else:
+            raise ValueError
     
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
 
-    # реалізація класу
+    def find_phone(self, phone):
+        for user_phone in self.phones:
+            if user_phone.value == phone:
+                return user_phone
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict):
-    # реалізація класу
+    def __int__(self):
+        self.data = dict()
+    def add_record(self, record):
+        name = record.name.value
+        self.data[name] = record
