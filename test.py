6e06da6c364e470d9da255a3be5ba6908a5286fc
sersim33 <sersim33@gmail.com>
2024-03-04 12:34:47 +0200

    com2

--- test.py
+++
@@ -1,7 +0,0 @@
-def len_phone(phone):
-    if len(phone) == 10 and phone.isdigit():
-        return phone
-    else:
-        print(f"Your phone number {phone} is not correct")
-
-print(len_phone("78787878"))
